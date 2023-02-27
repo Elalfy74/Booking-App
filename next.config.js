7766ba99c5eb741598f6a624ade8d9608120c5d0
Elalfy74 <62221802+Elalfy74@users.noreply.github.com>
2023-02-27 03:53:17 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.BACKEND_URL}/:path*`   }
+    ]
+  },
   publicRuntimeConfig: {
     backendUrl: process.env.BACKEND_URL,
   },
